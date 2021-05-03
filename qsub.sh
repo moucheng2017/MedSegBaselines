9702aa065ba6565dd78e1a1f1ac6c3613668bd3a
moucheng2017 <rmapmxu@ucl.ac.uk>
2021-05-03 13:32:56 +0100

    change working directory in qsub

--- qsub.sh
+++ qsub.sh
@@ -2,6 +2,6 @@
 #$ -l gpu=true
 #$ -S /bin/bash
 #$ -j y
-#$ -wd /cluster/project2/Neuroblastoma/SegTemplate
+#$ -wd /cluster/project2/Neuroblastoma/MedSegBaselines
 
 ~/anaconda3/envs/env2021/bin/python Run_test.py
